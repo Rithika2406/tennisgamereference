9cd57e0a710bb35f36e2b0deec2bfe6ee3caeb8e
Rithika P <166213945+Rithika2406@users.noreply.github.com>
2025-01-09 18:30:17 +0530

    Add files via upload

---
+++ tennisgamereference.java
@@ -0,0 +1,27 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner S=new Scanner(System.in);
+		int t=S.nextInt();
+		while(t-->0)
+		{
+		    int a=S.nextInt();
+		    int b=S.nextInt();
+		    int c=S.nextInt();
+		    int d=S.nextInt();
+		    if(a==0 && b==0 && c==0 && d==0)
+		    {
+		        System.out.println("IN");
+		    }
+		    else{
+		        System.out.println("OUT");
+		    }
+		}
+
+	}
+}
